436a66c6cb2f3662b4893cfa79eb50c8b18968ca
Julian Belistri <juebel@gmail.com>
2020-03-26 23:00:31 +0000

    Conf

--- config/gde.php
+++ config/gde.php
@@ -2,31 +2,13 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
-    |
-    */
 
     'securityUrl' => env('GDE_SECURITYURL', 'http://msec.tst.gde.gob.ar/gde-security/security/auth'),
     'application' => env('GDE_APPLICATION', 'R0VET19BUFBMSUNBVElPTg=='),
     'access_provider' => env('GDE_ACCESS_PROVIDER', 'CASSimple'),
-    'username' => env('GDE_USERNAME', 'btusca'),
-    'password' => env('GDE_PASSWORD', 'Gdetst.6'),
-    
+    'username' => env('GDE_USERNAME', 'TAD_QR'),
+    'password' => env('GDE_PASSWORD', 'Generico1'),
     'pdfApiUrl' => env('GDE_PDFAPIURL', 'http://eugapi.tst.gde.gob.ar/gde-restfull-api-web/api/v1/documento/'),
-
-    
-    
-
-   
-
-   
    
 
 ];
